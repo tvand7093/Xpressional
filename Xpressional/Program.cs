ecdb1870dc5af8f6e2ed7c31a7ad9afafcec4ed3
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-10-08 01:32:06 -0700

    Prettify the output of errors.

--- Xpressional/Program.cs
+++ Xpressional/Program.cs
@@ -7,25 +7,38 @@ namespace Xpressional
 {
 	class MainClass
 	{
-		static void DoWork(string[] args){
+		static void ProcessDocument(string[] args){
 			var output = new OutputManager (new ConsoleHelper ());
 			var fileContents = File.ReadAllLines (args.Length != 0 ? args[0] : "test_expressions.txt");
 			var inputManager = new InputManager ();
 
 			foreach (var line in fileContents) {
+				Console.ForegroundColor = ConsoleColor.Yellow;
 				Console.WriteLine ("======= BEGIN GRAPH =======");
 				Console.WriteLine ("Input Expression: " + line);
+				Console.ResetColor ();
 
 				var outputGraph = inputManager.ParseExpression (line);
 				output.Print (outputGraph);
 
+				Console.ForegroundColor = ConsoleColor.Yellow;
 				Console.WriteLine ("======= END GRAPH =======");
+				Console.ResetColor ();
+
 			}
 		}
 
 		public static void Main (string[] args)
 		{
-			DoWork (args);
+			try{
+				ProcessDocument (args);
+			}
+			catch (Exception e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine ("There was an error while processing the document: " + e.Message);
+				Console.ResetColor ();
+			}
 		}
 	}
 }
